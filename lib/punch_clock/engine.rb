373d42bf6ea3f1bd32cfa200cc28496eddd142a9
Miguel Salas <miguel@kundigo.pro>
2017-11-13 18:44:22 -0400

    feat: added block to setup rspec and factory_bot for tests.

--- lib/punch_clock/engine.rb
+++ lib/punch_clock/engine.rb
@@ -1,5 +1,10 @@
 module PunchClock
   class Engine < ::Rails::Engine
     isolate_namespace PunchClock
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_bot, :dir => 'spec/factories'
+    end
   end
 end
