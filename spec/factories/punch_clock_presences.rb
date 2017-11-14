189525c51983bfce5e1738266bd8aa28fa66d09f
Miguel Salas <miguel@kundigo.pro>
2017-11-14 14:09:14 -0400

    chore: migrates factory code to the engine project.

--- spec/factories/punch_clock_presences.rb
+++ spec/factories/punch_clock_presences.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :punch_clock_presence, class: 'PunchClock::Presence' do
-    
+    status "online"
+    last_time_browser_open   { Time.now.utc }
+    last_time_browser_active { Time.now.utc }
   end
 end
