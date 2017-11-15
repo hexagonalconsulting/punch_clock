6f64c80734c049a96ca06de6785fb7dbfaca14aa
Miguel Salas <miguel@kundigo.pro>
2017-11-15 13:23:48 -0400

    chore: copies over worker. note: the presence model is now prefixed with the engine module.

---
+++ app/workers/user_activity/user_report_offline_worker.rb
@@ -0,0 +1,18 @@
+module UserActivity
+  class UserReportOfflineWorker
+    include Sidekiq::Worker
+    sidekiq_options :retry => 0
+
+    def perform
+
+      idle_presences = PunchClock::Presence.where(status: 'idle').or(PunchClock::Presence.where(status: 'online'))
+
+      idle_presences.each do |presence|
+        if presence.should_be_offline?
+          presence.set_offline
+        end
+      end
+
+    end
+  end
+end
