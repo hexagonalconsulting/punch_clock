add79dd68a9572d7f230358154f13ec629a3fb2c
Miguel Salas <miguel@kundigo.pro>
2017-11-21 13:59:21 -0400

    chore: configuration for action cable mount path, it seems '/cable' is the default path, but just in case I include it in the test configuration.

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # seems it connect to this path by default
+  #config.action_cable.mount_path = '/cable'
 end
