51aeedefe08b90bb3e819e36d016bdc1498f6038
Miguel Salas <miguel@kundigo.pro>
2017-11-23 16:07:09 -0400

    fix: verify user using warden in the host app.

--- test/dummy/app/channels/application_cable/connection.rb
+++ test/dummy/app/channels/application_cable/connection.rb
@@ -9,7 +9,7 @@ module ApplicationCable
     end
 
     def find_verified_user # this checks whether a user is authenticated with devise
-      if verified_user = User.find_by(id: cookies.signed['user.id'])
+      if verified_user = env['warden'].user
         verified_user
       else
         reject_unauthorized_connection
