90d5d89215c3ff1bd46c0e359e2435b5c289a4e1
Miguel Salas <miguel@kundigo.pro>
2017-11-23 16:09:42 -0400

    chore: mount the sidekiq web interface in the host app routes.

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   mount PunchClock::Engine => "/punch_clock"
   get '/examples' => 'examples#index'
   root to: 'examples#index'
+  mount Sidekiq::Web => '/sidekiq'
 end
