3aa306dc90d0f62248916c8670fb14e352d7293a
Miguel Salas <miguel@kundigo.pro>
2017-11-23 16:08:54 -0400

    chore: include interval calls to in the user activity channel js code.

--- app/assets/javascripts/channels/punch_clock/user_activity.js
+++ app/assets/javascripts/channels/punch_clock/user_activity.js
@@ -2,19 +2,62 @@
 // if there is a current_user from devise.
 if (document.querySelectorAll('meta[name=action-cable-url]').length) {
 
-    App.user_activity = App.cable.subscriptions.create("PunchClock::UserActivityChannel", {
+    var reportBrowserAsActive;
+
+    App.userActivitySubscription = App.cable.subscriptions.create("PunchClock::UserActivityChannel", {
+
         connected: function() {
             // Called when the subscription is ready for use on the server
             console.log('subscribed to PunchClock::UserActivityChannel')
         },
 
         disconnected: function() {
-            // Called when the subscription has been terminated by the server
+            //Called when the subscription has been terminated by the server.
         },
 
         received: function(data) {
-            // Called when there's incoming data on the websocket for this channel
+            //Called when there's incoming data on the websocket for this channel.
+        },
+
+        report_browser_as_active: function() {
+            return this.perform('report_browser_as_active');
+        },
+
+        report_browser_as_open: function() {
+            return this.perform('report_browser_as_open');
         }
+
     });
 
+    //!App.userActivitySubscription.consumer.connection.disconnected means that the user is not disconnected (logged out).
+
+    reportBrowserAsActive = function() {
+
+        if (!App.userActivitySubscription.consumer.connection.disconnected) {
+            return App.userActivitySubscription.report_browser_as_active();
+        }
+
+    };
+
+
+
+    document.onclick = function() {
+        return reportBrowserAsActive();
+    };
+
+    document.onkeypress = function() {
+        return reportBrowserAsActive();
+    };
+
+    window.onfocus = function() {
+        return reportBrowserAsActive();
+    };
+
+    var reportInterval =  5000;
+
+    setInterval(function () {
+        if (!App.userActivitySubscription.consumer.connection.disconnected) {
+            return App.userActivitySubscription.report_browser_as_open();
+        }
+    }, reportInterval);
 }
