f0cf24faf200334f22846813ca495227463fe6eb
Miguel Salas <miguel@kundigo.pro>
2017-11-24 02:03:17 -0400

    fix: wraps the whole code in the validation for the action cable metatag

--- test/dummy/app/assets/javascripts/channels/admin_supervison.js
+++ test/dummy/app/assets/javascripts/channels/admin_supervison.js
@@ -1,39 +1,40 @@
+if (document.querySelectorAll('meta[name=action-cable-url]').length) {
+    var userStatus = 'whatever';
+    var  userId = 1;
+    App['PunchClock::ActivitySupervisionChannel:userId[' + userId + ']'] = App.cable.subscriptions.create({
+        channel: "PunchClock::ActivitySupervisionChannel",
+        id: userId}, {
+        received: function(userUpdate)  {
+            if ( window.AppInfo.railsEnv === 'development' || window.AppInfo.railsEnv === 'test' ) {
+                userStatus = userUpdate.status;
+                console.log('userStatus:', userStatus);
 
-var userStatus = 'whatever';
-var  userId = 1;
-App['PunchClock::ActivitySupervisionChannel:userId[' + userId + ']'] = App.cable.subscriptions.create({
-    channel: "PunchClock::ActivitySupervisionChannel",
-    id: userId}, {
-    received: function(userUpdate)  {
-        if ( window.AppInfo.railsEnv === 'development' || window.AppInfo.railsEnv === 'test' ) {
-            userStatus = userUpdate.status;
-            console.log('userStatus:', userStatus);
+                var userStatusMessage = 'user with the id: ' + userUpdate.id + ' has the status: ' + userStatus;
+                document
+                    .getElementById('admin-supervision-user-status')
+                    .insertAdjacentHTML('beforeend', userStatusMessage );
 
-            var userStatusMessage = 'user with the id: ' + userUpdate.id + ' has the status: ' + userStatus;
+            }
+        },
+        connected: function() {
+            // Called when the subscription is ready for use on the server
+            if ( window.AppInfo.railsEnv === 'development' || window.AppInfo.railsEnv === 'test' ) {
+                var activitySupervisionSubscriptionMessage = 'you are subscribed to PunchClock::ActivitySupervisionChannel';
+                console.log(activitySupervisionSubscriptionMessage);
+                document
+                    .getElementById('admin-supervision-subscription-message')
+                    .insertAdjacentHTML('beforeend', activitySupervisionSubscriptionMessage );
+            }
+
+        },
+        rejected: function() {
+
+            var rejectedMessage = 'you were rejected from the PunchClock::ActivitySupervisionChannel';
+            console.log(rejectedMessage);
             document
-                .getElementById('admin-supervision-user-status')
-                .insertAdjacentHTML('beforeend', userStatusMessage );
+                .getElementById('admin-supervision-rejected-message')
+                .insertAdjacentHTML('beforeend', rejectedMessage );
 
         }
-    },
-    connected: function() {
-        // Called when the subscription is ready for use on the server
-        if ( window.AppInfo.railsEnv === 'development' || window.AppInfo.railsEnv === 'test' ) {
-            var activitySupervisionSubscriptionMessage = 'you are subscribed to PunchClock::ActivitySupervisionChannel';
-            console.log(activitySupervisionSubscriptionMessage);
-            document
-                .getElementById('admin-supervision-subscription-message')
-                .insertAdjacentHTML('beforeend', activitySupervisionSubscriptionMessage );
-        }
-
-    },
-    rejected: function() {
-
-        var rejectedMessage = 'you were rejected from the PunchClock::ActivitySupervisionChannel';
-        console.log(rejectedMessage);
-        document
-            .getElementById('admin-supervision-rejected-message')
-            .insertAdjacentHTML('beforeend', rejectedMessage );
-
-    }
-});
+    });
+}
