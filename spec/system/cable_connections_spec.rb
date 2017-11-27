0e5cb4c1834e9ba654017e2387244da2f8982d0e
Miguel Salas <miguel@kundigo.pro>
2017-11-26 23:37:10 -0400

    fix: analyzes the test for the super admin roles into different more specific examples.

--- spec/system/cable_connections_spec.rb
+++ spec/system/cable_connections_spec.rb
@@ -13,21 +13,39 @@ module PunchClock
 
       find_button('Sign up').click
 
-    end
-
-
-    it 'shows evidence of connection to the channels' do
-
       visit root_url
 
-      click_on('Button') #TODO: find out why status 'online' is not shown even when clicking in something.
-      # the weird thing is that if you click while the test driven browser shows up, the status 'online' shows up too.
-
-      expect(page).to have_content('you are subscribed to PunchClock::UserActivityChannel')
-      expect(page).to have_content('you are subscribed to PunchClock::ActivitySupervisionChannel')
-      expect(page).to have_content('user with the id: 1 has the status: offline')
-
     end
 
+
+    describe 'evidence of connection to the channels' do
+
+
+      describe 'users with superadmin role can connect to the superadmins channel' do
+
+        before do
+          # let the user be a superadmin
+          user = User.first
+          user.superadmin = true
+          user.save
+        end
+
+        it 'can connect to the users UserActivityChannel' do
+          expect(page).to have_content('you are subscribed to PunchClock::UserActivityChannel')
+        end
+
+        it 'can connect to the ActivitySupervisionChannel' do
+          expect(page).to have_content('you are subscribed to PunchClock::ActivitySupervisionChannel')
+        end
+
+        it 'get updates from a user using the subscription to the ActivitySupervisionChannel' do
+          #click_on('Button') #TODO: find out why status 'online' is not shown even when clicking in something.
+          # the weird thing is that if you click while the test driven browser shows up, the status 'online' shows up too.
+          expect(page).to have_content('user with the id: 1 has the status: offline')
+        end
+
+      end
+
+    end
   end
 end
