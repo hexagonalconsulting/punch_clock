3911dcc5f7281aba25bd4d6a4064ea5a7ee94af4
Miguel Salas <miguel@kundigo.pro>
2017-11-27 20:32:37 -0400

    fix: login the user with :login_as  from Warden::Test::Helpers.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,6 +29,9 @@ Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+
+  config.include Warden::Test::Helpers
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
--- spec/system/cable_connections_spec.rb
+++ spec/system/cable_connections_spec.rb
@@ -1,31 +1,15 @@
 require 'rails_helper'
 
 module PunchClock
-  RSpec.describe 'Cable connections', type: :system, js: true do
+  RSpec.describe 'Cable connections', type: :feature, js: true do
 
-    before do
+    let!(:user) { create(:user) }
 
-      visit '/users/sign_up'
-
-      fill_in('Email',    with: 'fancyemail@domain.com')
-      fill_in('Password', with: 'super_secret!')
-      fill_in('Password confirmation', with: 'super_secret!')
-
-      find_button('Sign up').click
-
-      visit examples_url
-
-      if current_path != examples_path && current_path == '/users/sign_in'
-        fill_in('Email',    with: 'fancyemail@domain.com')
-        fill_in('Password', with: 'super_secret!')
-        find_button('Log in').click
-      end
-
-    end
+    before { login_as(user, :scope => :user) }
 
     describe 'evidence of connection to the channels' do
 
-      before { visit examples_url }
+      before { visit examples_path }
 
       it 'is in the right path, the one that all other test will be using' do
         expect(page).to have_current_path(examples_path)
@@ -49,12 +33,7 @@ module PunchClock
 
       describe 'users with superadmin role' do
 
-        before do
-          # let the user be a superadmin
-          user = User.first
-          user.superadmin = true
-          user.save
-        end
+        let!(:user) { create(:user, superadmin: true) }
 
         it 'can connect to the users UserActivityChannel' do
           expect(page).to have_content('you are subscribed to PunchClock::UserActivityChannel')
@@ -65,9 +44,7 @@ module PunchClock
         end
 
         it 'get updates from a user using the subscription to the ActivitySupervisionChannel' do
-          #click_on('Button') #TODO: find out why status 'online' is not shown even when clicking in something.
-          # the weird thing is that if you click while the test driven browser shows up, the status 'online' shows up too.
-          expect(page).to have_content('user with the id: 1 has the status: offline')
+          expect(page).to have_content('user with the id: 1 has the status: online')
         end
 
       end
